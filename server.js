68ced4f8c20324b3f61c54028f88432f35045766
whitecodel <henryroy40@gmail.com>
2022-07-08 07:49:31 +0530

    final raw learning

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const http = require("http");
 // Create a server
 http
   .createServer(function (request, response) {
-    // Send the HTTP header field "Content-type" with value "text/html"
-    response.writeHead(200, { "Content-Type": " text/html" });
+    // // Send the HTTP header field "Content-type" with value "text/html"
+    response.writeHead(200, { "Content-Type": "text/html" });
 
     // Send the response body as "Hello World"
-    response.end("Hello World\n");
+    switch (request.url) {
+      case "/x":
+        response.end("X Hello World\n");
+        break;
+      case "/y":
+        response.end("Y Hello World\n");
+        break;
+      case "/z":
+        response.end("Z Hello World\n");
+        break;
+      default:
+        response.end("Hello World\n");
+        break;
+    }
   })
   .listen(8080);
